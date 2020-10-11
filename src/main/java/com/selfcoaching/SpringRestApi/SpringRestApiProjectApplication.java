84cf5dd075f03695ffea26718fc7114964b4c13f
christosponirakos <cponirakos@gmail.com>
2020-10-11 18:52:20 +0300

    first commit. pom and basic directory structure

---
+++ src/main/java/com/selfcoaching/SpringRestApi/SpringRestApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.selfcoaching.SpringRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfcoaching/SpringRestApi/SpringRestApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfcoaching.SpringRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
