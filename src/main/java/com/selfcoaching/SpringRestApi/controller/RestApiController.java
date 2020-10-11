78ee04837f0cdc1fb9963f90dec5bc2234ef42a9
christosponirakos <cponirakos@gmail.com>
2020-10-11 19:17:55 +0300

    adding the logger

--- src/main/java/com/selfcoaching/SpringRestApi/controller/RestApiController.java
+++ src/main/java/com/selfcoaching/SpringRestApi/controller/RestApiController.java
@@ -5,10 +5,18 @@
  */
 package com.selfcoaching.SpringRestApi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Chris
  */
+
+@RestController
+@RequestMapping("/api")
 public class RestApiController {
-    
+    public static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
 }
